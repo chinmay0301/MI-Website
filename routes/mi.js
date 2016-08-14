46c55e716808efdba7460dd4db6c90aa0c2cd14a
Chinmay Talegaonkar <chinmay0301@gmail.com>
2016-08-14 19:05:02 +0530

    password hide

--- routes/mi.js
+++ routes/mi.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'cat@03$01$98',
+  password : '*****',
   database : 'MI'
 });
 
