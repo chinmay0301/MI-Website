825ebee6ceff6e1d747dc209c1ccc8d77ef4cfb9
chinmay <chinmay0301@gmail.com>
2016-08-14 15:15:01 +0530

    title changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'MoodI 2k16' });
 });
 
 module.exports = router;
